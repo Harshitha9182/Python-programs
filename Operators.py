52b574aef919d8d1daac1e9ea6fa9664f902e8bf
Harshitha Buchupalle <harshithabuchupalle@gmail.com>
2026-02-20 19:36:44 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,93 @@
+#Arithmetic operator
+'''x = 4
+y = 5
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(x ** y)
+nnn
+#print(x+y,x   -y,x*y,x/y,x%y,x//y,x**y)
+x = 12
+y =9
+print(x%y)'''
+
+
+#Assignment operator
+'''a=4
+print(a)
+a+=2
+print(a)
+a+=1
+print(a)
+a-=1
+print(a)
+a*=2
+print(a)
+a/=2
+print(a)
+a//=2
+print(a)
+a%=2
+print(a)
+a**=3
+print(a)
+
+x=2
+y=3
+print(x)
+print(y)
+x+=y
+print(x)
+y+=x
+print(y)'''
+
+
+#Bitwise operator
+'''
+print(2&3)
+print(2|3)
+print(2^3)
+x=4
+y=5
+print(x&y)
+print(x|y)
+print(x^y)'''
+
+
+#Comparison operator
+'''
+a=30
+b=12
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+print(a>b)
+print(a<b)'''
+
+#Logical Operator
+'''
+a=20
+b=23
+print(a>b and a<b)
+print(a<=b and a!=b)
+print(a<b or a==b)
+print(a==b or a>=b)
+print(not(a>b))
+print(not(a!=b))'''
+
+#Special Operators
+#Identity Operator
+'''
+a=2
+b=3
+print(a is b)
+print(a is not b)'''
+#Membership operator
+
+a=[1,2,3,4]
+for i in a:
+    print(i)
