cb99efc958465dbe42046f7a8fa1e5788eed7169
Harshitha Buchupalle <harshithabuchupalle@gmail.com>
2026-02-20 19:39:51 +0530

    Add files via upload

---
+++ Type_casting.py
@@ -0,0 +1,41 @@
+ #1.Get input for variable name and age.print it.
+'''
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+print("My Name is:",name)
+print("My age is:",age)
+
+'''
+#2.Get for variable name and age,address.print it.
+'''
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+address=input("Enter your address:")
+print("My Name is:",name)
+print("My age is:",age)
+print("My address is:",address)
+'''
+'''
+address=input("Enter your address:")
+print("My address is:",address)
+'''
+
+#3.Get integer input for variable a,b,c.
+#Multiply all 3 variables(a*b*c)
+#Add all 3 variable(a+b+c)
+#Divide the multiplied valu
+'''
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+c=int(input("Enter c:"))
+print("Multiplication of (a*b*c):",(a*b*c))
+print("Addition of (a+b+c):",(a+b+c))
+print("Division:",(a*b*c)/(a+b+c))
+'''
+w1=input()
+w2=input()
+w3=input()
+print(w1[0])
+print(w2[0])
+print(w3[0])
+
